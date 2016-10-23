837fc3f2de1fa3c933099c051e95ddf3cfdc9ac4
andersonblopes <ander@DESKTOP-39O3K7V>
2016-10-23 20:40:28 -0300

    Aula 06 - Passando argumentos pela linha de comando

---
+++ src/com/javabasico/aula06/Argumentos.java
@@ -0,0 +1,9 @@
+package com.javabasico.aula06;
+
+public class Argumentos {
+
+	public static void main(String[] args) {
+		System.err.println("Você digitou: " + args[0] + " - " + args[1] + " - " + args[2]);
+	}
+
+}
