a9ac24cdb0b2cab176d878906812e95cf6a0d980
andersonblopes <ander@DESKTOP-39O3K7V>
2016-10-23 21:03:29 -0300

    Exemplo de erros de sintaxe, compilação e execução.

---
+++ src/com/javabasico/aula07/Erro02.java
@@ -0,0 +1,11 @@
+package com.javabasico.aula07;
+
+public class Erro02 {
+
+	public static void main(String[] args) {
+		int cont;
+		System.out.println("Você digitou: " + cont);
+
+	}
+
+}
---
+++ src/com/javabasico/aula07/Erro03.java
@@ -0,0 +1,8 @@
+package com.javabasico.aula07;
+
+public class Erro03 {
+	public static void Main(String[] args) {
+		System.out.println("Você digitou: " + 1/0);
+	}
+
+}
